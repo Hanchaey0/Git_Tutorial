c8ff44303baa15f96fa23f81dd386301be60822a
seulgi-i <4seul1@daum.net>
2022-09-30 21:35:21 +0900

    upload files

--- spring_pj_117_csg/src/main/java/util/SettingValues.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public interface SettingValues {
-	   // 이메일 계정정보 - naver
-	   public static String ADMIN = "tmfrl043@naver.com";  // 본인 이메일 계정
-	   public static String PW = "Seulgi94!!!";  // 본인 이메일 패스워드
-	   
-	}     
